fd7949116548f4274bc8f859f3e11ee4e03178b5
Ole Goltermann <goltermann@nin.uke.uni-hamburg.de>
2023-01-27 18:27:25 +0100

    finalize conv_3D_4D script, but see TODOs in script

--- analysis/preprocessing/functions/nii_conv_3D_to_4D.m
+++ analysis/preprocessing/functions/nii_conv_3D_to_4D.m
@@ -16,8 +16,7 @@ function nii_conv_3D_to_4D(all_sub_id)
 % Quick and dirty script --> clean up and write it as function 
 %   TODO 1: automatize dummy calculation and include error message
 %   TODO 2: loop to get dir names could be included in a function
-%   TODO 3: not sessions but runs -> no extra folder needed
-%   TODO 4: naming convention -> nulled & not nulled instead of VASO/BOLD
+%   TODO 3: integrate parallel processing
 % ________________________________________________________________________________________
 
 % ---------- set paths -------------------------------------------------------------------
@@ -37,11 +36,11 @@ for s = 1:subs
     subjects(s) = str2double(regexp(string(subDirsNames(s)),'\d*','Match'));
 end
 
-% get amount of sessions for each subject
-ses = zeros(1,subs);
+% get amount of runs for each subject
+run = zeros(1,subs);
 for s = 1:subs
-    dir_ses = dir([raw_func_dir '/' char(subDirsNames(s)) '/ses*']);
-    ses(s) = numel(dir_ses([dir_ses(:).isdir]));
+    dir_run = dir([raw_func_dir '/' char(subDirsNames(s)) '/run*']);
+    run(s) = numel(dir_run([dir_run(:).isdir]));
 end
 
 %---------- if not done already exclude dummies ------------------------------------------
@@ -49,16 +48,16 @@ end
 dummy = 3; % 3 for VASO, 3 for BOLD
 
 for s = 1:subs
-    for b = 1:ses(s)
-        dir_ses = fullfile(raw_func_dir, sprintf('/sub-0%d/ses-0%d',s,b));
-        % get names of the folders inside session directory (VASO/BOLD, magn/phas)
-        files = dir(dir_ses);
+    for b = 1:run(s)
+        dir_run = fullfile(raw_func_dir, sprintf('/sub-0%d/run-0%d',s,b));
+        % get names of the folders inside run directory (VASO/BOLD, magn/phas)
+        files = dir(dir_run);
         dir_log= [files.isdir];
         sub_folders = files(dir_log);
         subFolderNames = {sub_folders(3:end).name};
         % acess the VASO/BOLD, magn/phas folders
         for f = 1:length(subFolderNames)
-            dir_seq = fullfile(dir_ses, char(subFolderNames(f)));
+            dir_seq = fullfile(dir_run, char(subFolderNames(f)));
             dir_dum = fullfile(dir_seq, 'dummy');
             % select all files in dir_seq folder
             files = spm_select('FPList',dir_seq,'.nii');
@@ -71,7 +70,8 @@ for s = 1:subs
                     movefile(dummies(i,:),dir_dum);
                 end
             else
-                fprintf('Already a dummy folder')
+                fprintf('Already a dummy folder for sub-0%d_%s\n', ...
+                    s,char(subFolderNames(f)))
             end
         end
     end
@@ -79,51 +79,29 @@ end
 
 %---------- create 4D nifti --------------------------------------------------------------
 
-prefix = {'f'};
-
 for s = 1:subs
     dir_save = fullfile(dir_der, sprintf('/spm_preproc/sub-0%d/func/',s));
     % check if 4D folder is already there
     if not(isfolder(dir_save))
         mkdir(dir_save);
-        for b = 1:ses(s)
-            dir_ses = fullfile(raw_func_dir, sprintf('/sub-0%d/ses-0%d',s,b));
-            % get names of the folders inside session directory (VASO/BOLD, magn/phas)
-            files = dir(dir_ses);
+        for b = 1:run(s)
+            dir_run = fullfile(raw_func_dir, sprintf('/sub-0%d/run-0%d',s,b));
+            % get names of the folders inside run directory (VASO/BOLD, magn/phas)
+            files = dir(dir_run);
             dir_log= [files.isdir];
             sub_folders = files(dir_log);
             subFolderNames = {sub_folders(3:end).name};
             % acess the VASO/BOLD, magn/phas folders
             for f = 1:length(subFolderNames)
-                dir_seq = fullfile(dir_ses, char(subFolderNames(f)));
-            
-            
-
-
-    for b = 1:blocks(s)
-        savedir = fullfile(func_dir,sprintf('/Run%d',b));
-        if not(isfolder(savedir))
-        mkdir(savedir)
-        end
-        rundir = fullfile(subdir,sprintf('/Raw/Run%d',b));
-        for m = 1:numel(prefix)
-            pre = char(prefix(m));
-            files = spm_select('FPList', rundir,['^' pre 'PRISMA.*.nii$']);
-            fprintf('creating 4D nii for %s, Run0%d\n', char(subDirsNames(s)), b);
-            scans = spm_vol(files);
-            try
-                if m == 1
-                    dlmwrite(fullfile(savedir,'3Dto4Dfiles.txt'),files,'delimiter','');
-                end
-                spm_file_merge(scans,fullfile(savedir,sprintf('%sMRI_Sub%02.2d_r%d.nii',pre,subjects(s),b))); %convert to 4D nifti
-            catch
-                disp(['filemerge: problems with Sub' num2str(subjects(b))]);
+                dir_seq = fullfile(dir_run, char(subFolderNames(f)));
+                files = spm_select('FPList', dir_seq,'^fPRISMA.*.nii$');
+                scans = spm_vol(files);
+                spm_file_merge(scans,fullfile(dir_save,[char(subFolderNames(f)),'.nii']));
             end
             clear scans files
         end
-    end
+    else
+        fprintf('Already converted for sub-0%d\n',s)
     end
 end
 
-
-% <run1>_[nulled|notnulled].nii

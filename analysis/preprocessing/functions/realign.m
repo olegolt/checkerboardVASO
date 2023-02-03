8676a87ed836a10d23dfed59f16b04d618d62abe
Ole Goltermann <goltermann@nin.uke.uni-hamburg.de>
2023-02-03 16:47:15 +0100

    realig script

--- analysis/preprocessing/functions/realign.m
+++ analysis/preprocessing/functions/realign.m
@@ -21,12 +21,13 @@ function realign(all_sub_id)
 % TODO: 
 % Quick and dirty script --> clean up and write it as function 
 % TODO 1: integrate path function 
+% TODO 2: run_paralell
 % ________________________________________________________________________________________
 
 % ---------- set paths -------------------------------------------------------------------
 
 dir_der = '/home/goltermann/checkerboardVASO/data/derivatives/spm_preproc';
-
+dir_raw_func = '/home/goltermann/checkerboardVASO/data/raw/func';
 %---------- prepare folders for later loops ----------------------------------------------
 
 % get amount of subjects and save names into a cell array
@@ -37,3 +38,71 @@ subjects = zeros(1,subs);
 for s = 1:subs
     subjects(s) = str2double(regexp(string(subDirsNames(s)),'\d*','Match'));
 end
+
+% get amount of runs for each subject
+run = zeros(1,subs);
+for s = 1:subs
+    dir_run = dir([dir_raw_func '/' char(subDirsNames(s)) '/run*']);
+    run(s) = numel(dir_run([dir_run(:).isdir]));
+end
+
+%---------- realignment of nulled (VASO) -------------------------------------------------
+
+matlabbatch = cell(1,subs);
+
+for s=1:subs
+    dir_sub = fullfile(dir_der,sprintf('sub-%02.2d/func',subjects(s)));
+    for b=2:run(s)
+        mask = fullfile(dir_sub,'mask.nii');
+        run_pattern = sprintf('run_%d_nulled_magn.nii',b);
+        files{b-1} = cellstr(spm_select('ExtFPList', dir_sub, run_pattern));
+        % create batch for estimate & write 
+        matlabbatch{s}.spm.spatial.realign.estwrite.data             = files;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.quality = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.sep     = 1.2;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.fwhm    = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.rtm     = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.interp  = 7;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.wrap    = [0 0 0];
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.weight  = {mask};
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.which   = [2 1];
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.interp  = 7;
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.wrap    = [0 0 0];
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.mask    = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.prefix  = 'mc_';
+    end
+end
+
+spm_jobman('run',matlabbatch);
+clear matlabbatch files
+
+%---------- realignment of notnulled (BOLD) -------------------------------------------------
+
+matlabbatch = cell(1,subs);
+
+for s=1:subs
+    dir_sub = fullfile(dir_der,sprintf('sub-%02.2d/func',subjects(s)));
+    for b=2:run(s)
+        mask = fullfile(dir_sub,'mask.nii');
+        run_pattern = sprintf('run_%d_notnulled_magn.nii',b);
+        files{b-1} = cellstr(spm_select('ExtFPList', dir_sub, run_pattern));
+        % create batch for estimate & write 
+        matlabbatch{s}.spm.spatial.realign.estwrite.data             = files;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.quality = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.sep     = 1.2;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.fwhm    = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.rtm     = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.interp  = 7;
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.wrap    = [0 0 0];
+        matlabbatch{s}.spm.spatial.realign.estwrite.eoptions.weight  = {mask};
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.which   = [2 1];
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.interp  = 7;
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.wrap    = [0 0 0];
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.mask    = 1;
+        matlabbatch{s}.spm.spatial.realign.estwrite.roptions.prefix  = 'mc_';
+    end
+end
+
+spm_jobman('run',matlabbatch);
+clear matlabbatch files
+
